2bd812a20527dba354cf53d60da99b5a47320097
NP <nb.piscitello@gmail.com>
2017-11-21 11:22:26 -0500

    I forgot Verilog isn't C - need to think more in hardware!

--- main.v
+++ main.v
@@ -1,26 +1,60 @@
-`define STATE_T     unsigned [1:0]
-`define BOARD_ROWS  3
-`define BOARD_COLS  3
-`define ROW_T [(`BOARD_ROWS-1):0]
-`define COL_T [(`BOARD_COLS-1):0]
+// uncomment below or pass -DDEBUG at compile time for verbose output
+//`define DEBUG
 
-// cell states
-`define STATE_BLANK 0
-`define STATE_X     1
-`define STATE_O     2
+`define BOARD_ROWS    3
+`define BOARD_COLS    3
+
+`define CLOCK_T       unsigned [0:0]
+`define STATE_T       [1:0]
+`define BOARD_T       [(`BOARD_ROWS * `BOARD_COLS - 1):0]
+
+// is it a bitmask? Is it just counting? The world may never know!
+`define CELL_BLANK   0
+`define CELL_X       1
+`define CELL_O       2
 
 module main;
-  // board[row][column] = cell state
-  reg `STATE_T board `ROW_T `COL_T;
-  reg unsigned `ROW_T row;
-  reg unsigned `COL_T col;
+
+  reg `CLOCK_T clock = 0;
+
+  // 3D arrays don't work well - I'll just suck it up and use a 2D array.
+  reg `BOARD_T `STATE_T board;
+  reg unsigned [(`BOARD_ROWS - 1):0] row;
+  reg unsigned [(`BOARD_COLS - 1):0] col;
+
+  output_m out(clock, board);
 
   initial begin
-    // start with a blank board
     for( row = 0; row < `BOARD_ROWS; row = row + 1 ) begin
       for( col = 0; col < `BOARD_COLS; col = col + 1 ) begin
-        board[row][col] = `STATE_BLANK;
+        board[`BOARD_ROWS * row + col] = `CELL_BLANK;
       end
     end
   end
+
+  always begin
+`ifdef DEBUG
+    $display("clock: %d", clock);
+`endif
+    #1 clock = ~clock;
+  end
+
+endmodule
+
+module output_m(  input wire `CLOCK_T clock,
+                  input wire `BOARD_T `STATE_T board );
+
+  initial begin
+    $display("\n(0,0), (0,1), (0,2); (1,0), (1,1), (1,2); (2,0), (2,1), (2,2)\n");
+  end
+
+  always @( negedge clock ) begin
+    // man, I wish we could use ncurses
+    // since we can't, we're limited to one line of output so we can overwrite it
+    $write("%02d, %02d, %02d; %02d, %02d, %02d; %02d, %02d, %02d\015",
+      board[0], board[1], board[2], 
+      board[3], board[4], board[5],
+      board[6], board[7], board[8]);
+  end
+
 endmodule
